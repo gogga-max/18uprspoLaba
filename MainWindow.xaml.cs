68cc5ef79e081d4a40de8ad2358bbd4b83e8060a
Gfaddd <gfaddde@gmail.com>
2025-04-10 11:22:54 +0300

    Logic for loading hotels data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +26,10 @@ namespace _18uprspoLaba
 		public MainWindow()
 		{
 			InitializeComponent();
+			var client = new WebClient();
+			var response = client.DownloadString("http://127.0.0.1:63450/api/hotels");
+			CurrentHotel = JsonConvert.DeserializeObject<List<Hotel>>(response);
+			DataContext = this;
 		}
 	}
 }
